6569c19cf55709d5b03a3e5dddf78ab60e3bdcca
Bee Kim <bk249@njit.edu>
2017-07-22 14:23:37 -0400

    delete cookies

--- index.php
+++ index.php
@@ -55,5 +55,16 @@ switch($action) {
     case 'end_session':
         $_SESSION = array();
         session_destroy();
+        $name = session_name();
+        $expire = strtotime('-1 year');
+        $params = session_get_cookie_params();
+        $path = $params['path'];
+        $domain = $params['domain'];
+        $secure = $params['secure'];
+        $httponly = $params['httponly'];
+        setcookie($name,'',$expire,$path,$domain,$secure,$httponly);
+
+        include('cart_view.php');
+        break;
 }
 ?>
